bbbcc1372eabf8c479c6d0ec68bebbd3660446db
alerodriguezn <navarrocmo@gmail.com>
2024-06-17 00:05:54 -0600

    fixing

--- src/app/api/appointment/route.ts
+++ src/app/api/appointment/route.ts
@@ -5,6 +5,8 @@ import { NextRequest } from "next/server";
 export async function POST(request: NextRequest) {
   const data = await request.formData();
 
+  console.log("xd");
+
 
   const make = data.get("make") as string;
   const model = data.get("model") as string;
@@ -12,81 +14,70 @@ export async function POST(request: NextRequest) {
   const clientId = data.get("clientId") as string;
   const workshopId = data.get("workshopId") as string;
   const description = data.get("description") as string;
-  const media = data.get("media");
-
-  return Response.json({ 
-    make,
-    model,
-    year,
-    clientId,
-    workshopId,
-    description,
-    media,
-    data
-  });
+  const media = data.get("media") as File;
 
 
-  // try {
-  //   const prismaTx = await prisma.$transaction(async (tx) => {
+  try {
+    const prismaTx = await prisma.$transaction(async (tx) => {
 
   
-  //     const newVehicle = await tx.vehicle.create({
-  //       data: {
-  //         make,
-  //         model,
-  //         year: Number(year),
-  //         ownerId: Number(clientId),
-  //       },
-  //     });
+      const newVehicle = await tx.vehicle.create({
+        data: {
+          make,
+          model,
+          year: Number(year),
+          ownerId: Number(clientId),
+        },
+      });
 
-  //     // new appointment
-  //     const newAppointment = await tx.appointment.create({
-  //       data: {
-  //         status: "Pending",
-  //         workshopId: Number(workshopId),
-  //         clientId: Number(clientId),
-  //         vehicleId: newVehicle.id,
-  //       },
-  //     });
+      // new appointment
+      const newAppointment = await tx.appointment.create({
+        data: {
+          status: "Pending",
+          workshopId: Number(workshopId),
+          clientId: Number(clientId),
+          vehicleId: newVehicle.id,
+        },
+      });
 
-  //     // new appointment detail
-  //     const newAppointmentDetail = await tx.appointmentDetail.create({
-  //       data: {
-  //         appointmentId: newAppointment.id,
-  //         description,
-  //       },
-  //     });
+      // new appointment detail
+      const newAppointmentDetail = await tx.appointmentDetail.create({
+        data: {
+          appointmentId: newAppointment.id,
+          description,
+        },
+      });
 
-  //     const uploadImage = async () => {
-  //       const blob = await put(media.name, media, {
-  //         access: "public",
-  //       });
+      const uploadImage = async () => {
+        const blob = await put(media.name, media, {
+          access: "public",
+        });
 
-  //       return blob;
-  //     };
+        return blob;
+      };
 
-  //     const imageUrl = await uploadImage();
+      const imageUrl = await uploadImage();
 
-  //     // new appointment media
-  //     await tx.appointmentMedia.create({
-  //       data: {
-  //         appointmentDetailId: newAppointmentDetail.id,
-  //         mediaUrl: imageUrl.url,
-  //       },
-  //     });
-  //     return newAppointment;
-  //   });
+      // new appointment media
+      await tx.appointmentMedia.create({
+        data: {
+          appointmentDetailId: newAppointmentDetail.id,
+          mediaUrl: imageUrl.url,
+        },
+      });
+      return newAppointment;
+    });
 
-  //   //execute transaction
-  //   await prismaTx;
+    //execute transaction
+    await prismaTx;
 
-  //   return Response.json({
-  //     message: "Appointment created successfully",
-  //   });
+    return Response.json({
+      message: "Appointment created successfully",
+    });
     
-  // } catch (error) {
-  //   return Response.json({ message: "Error creating appointment", error, data });
-  // }
+  } catch (error) {
+    return Response.json({ message: "Error creating appointment", error, data });
+  }
 }
 // Get All appointment by clientId
 
