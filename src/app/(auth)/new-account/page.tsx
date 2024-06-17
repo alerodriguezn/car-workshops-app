97e54521f46eb47d981bffe0b709da3599d63b94
alerodriguezn <navarrocmo@gmail.com>
2024-06-16 19:53:34 -0600

    chore: del spaces

--- src/app/(auth)/new-account/page.tsx
+++ src/app/(auth)/new-account/page.tsx
@@ -7,10 +7,7 @@ export default function NewAccountPage() {
     <main className="flex flex-col min-h-screen pt-32 sm:pt-52 sm:w-[350px]">
 
       <h1 className={ `text-4xl mb-5` }> New Account</h1>
-
       <RegisterForm/>
-
-      
     </main>
   );
 }
